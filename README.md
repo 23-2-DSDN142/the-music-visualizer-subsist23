2539967810e759fed5136be5a03bd25d9c22c325
subsist23 <dancrichton@hotmail.com>
2023-10-20 15:58:24 +1300

    Submission

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 A P5JS framework for running a simple music visualizer, intended as a creative coding I assignment.
 
 Song details: 
-Title = Just Dropped In (To See What Condition My Condition Was In)
-Artist = Kenny Rogers
+Title = A thousand kisses
+Artist = Vladimir Dubyshkin
+
--- music_starter.js
+++ music_starter.js
@@ -1,7 +1,7 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  if (bass< 50){
+  if (bass< 40){
     background	(112,128,144)} //grey
     else{
       background	(255,250,250)
@@ -26,6 +26,30 @@ let mappedXPost = map(drum+100,0,100,0,220)
 let mappedXPostbass =map(bass+200,0,100,0,100)
 
 //background 
+//horizonline
+
+
+noStroke();
+fill(123,104,238)
+rect(300,360,600,300)
+
+strokeWeight(3)
+stroke(176,224,230)
+fill(128,0,128)
+rect(300,380,200,30)
+rect(300,410,100,30)
+rect(300,710,80,630)
+rect(300,710,80,530)
+rect(300,710,80,530)
+rect(300,710,80,430)
+rect(300,710,80,330)
+rect(300,710,80,230)
+rect(300,710,80,130)
+rect(300,710,80,30)
+
+
+
+
 
 //SPEAKER
 //chord
@@ -33,15 +57,20 @@ noFill()
 strokeWeight(12)
 stroke(0)
 //ellipse(250,760,350,50)
-arc(50, 950, 180, 580, -110, PI + QUARTER_PI, OPEN);
+arc(50, 990, 230, 480, -90, PI + QUARTER_PI, OPEN);
 //mixer
+
 strokeWeight(9)
 stroke(176,224,230)
 fill(123,104,238)
 rect(110,600,220,320,50,50,0,0)
 noStroke()
-fill(128,0,128)
+fill(128,0,128) //darkpurple
 rect(110,600,180,260,50,50,0,0)
+noFill();
+stroke(0)
+rect(110,650,160,260,50,50,0,0)
+rect(110,600,120,220,50,50,0,0)
 //speaker
 stroke(176,224,230)
 fill(0)
@@ -63,7 +92,10 @@ ellipse(110,440,mappedXPostbass-190,5)
 stroke	(0,206,209)
 strokeWeight(6)
 fill (176,224,230)
-rect(300+170,80,160,60,)
+rect(300+170,80,160,60)
+fill(255)
+rect(300+170,80,80,30,23)
+rect(300+170,80,80,10,23)
 //arm
 stroke	(0,206,209)
 strokeWeight(6)
@@ -71,15 +103,22 @@ fill (176,224,230)
 rect(510,330,60,530,23)
 fill (255)
 rect(510,330,30,530,23)
+rect(510,330,10,530,23)
 //fingers
 fill(255)
 rect(340,615,120,35,25)
+rect(340,615,60,15,25)
 rect(320,555,120,35,25)
+rect(320,555,60,15,25)
 rect(340,495,120,35,25) 
+rect(340,495,60,15,25) 
 rect(440,455,120,35,25)
+rect(440,455,60,15,25)
 //hand
 fill(176,224,230)
 rect(420,555,180,160,25)
+rect(420,555,180,120,25)
+rect(420,555,130,120,25)
 //holeinhand
 fill	(223,100,100)
 ellipse(420,555,95)
@@ -99,21 +138,42 @@ rect(300,30,100,50,23)
 strokeWeight(9)
 fill(255)
 rect(300,80,230,90,12)
+strokeWeight(3)
+rect(300,80,190,70,12)
+rect(300,80,180,60,12)
+rect(300,80,140,40,12)
+rect(300,80,100,20,12)
 //bottomtorso
+strokeWeight(9)
 rect(300,230,110,60,12)
 //legleft
 rect(300-30,300,40,110)
 //legright
 rect(300+30,310,40,110)
+
+
 //shoes
 fill(0)
 rect(300+30,360,92,33,63,63,0,0)
 rect(300-30,360,42,33,63,63,0,0)
-//jar
+
+//puku
 stroke	(0,206,209)
 strokeWeight(16)
 fill	(176,224,230)
 rect(300,175,200,100,0,0,25,25)
+strokeWeight(3)
+noFill()
+rect(300,175,100,40,0,0,125,25)
+rect(300,175,100,50,0,0,25,25)
+rect(300,175,110,60,0,0,25,25)
+rect(300,175,120,70,0,0,25,25)
+rect(300,175,130,80,0,0,25,25)
+rect(300,175,140,90,0,0,25,25)
+rect(300,175,150,100,0,0,25,25)
+rect(300,175,160,110,0,0,25,25)
+rect(300,175,170,110,0,0,25,25)
+rect(300,175,180,110,0,0,25,25)
 //eyes
 noStroke()
 fill (127,255,212) //limegreen
@@ -124,14 +184,26 @@ stroke	(0,206,209)
 strokeWeight(6)
 fill (176,224,230)
 rect(300-150,60,230,60,25)
+fill(255)
+rect(300-150,60,200,25,25)
 //joints to arms
 fill (176,224,230)
 rect(mappedXPost-180,60,50,40,23)
 //middlearm
 fill (255)
 rect(mappedXPost-230,145,120,210,25)
+rect(mappedXPost-230,145,100,210,25)
+fill (176,224,230)
 rect(mappedXPost-230,145,80,210,25)
+fill(255)
+rect(mappedXPost-230,145,60,210,25)
+strokeWeight(3)
+fill (176,224,230)
+rect(mappedXPost-230,145,40,170,25)
+fill(255)
+rect(mappedXPost-230,145,20,120,25)
 //finger
+strokeWeight(6)
 rect(mappedXPost-195,355,26,bass+60,25)
 rect(mappedXPost-265,355,26,bass+60,25)
 rect(mappedXPost-230,355,26,bass+80,25)
@@ -140,14 +212,15 @@ fill(176,224,230)
 rect(mappedXPost-230,295,120,100,25)
 //thumb
 rect(mappedXPost-155,315,26,100,25)
-//orb
+//orb in hand
 fill(255)
 ellipse(mappedXPost-230,295,50)
+ellipse(mappedXPost-230,295,30)
 //heart
 noStroke()
-fill	(223,100,100)
+fill	(223,120,100)
 //rect(300,170,drum,drum,30)
-rect(300,170,30,30,30)
+rect(300,173,30,30,30)
 
 
 
